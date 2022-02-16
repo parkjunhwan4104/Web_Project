37aed919ebf21acea2dd5a117747324e368b62b7
parkjunhwan <wnsghks4104@gmail.com>
2022-02-16 14:51:07 +0900

    관리자 메인 페이지 구현

--- src/main/java/com/pjh/food_cm/controller/ADMController.java
+++ src/main/java/com/pjh/food_cm/controller/ADMController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/adm")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class ADMController {
     private final ADMService admService;  //final이 붙으면 초기화를 해줘야하는데  @RequiredArgsConstructor은 final이 붙은 것을 찾아서 생성자를 자동적으로 만들어줌
